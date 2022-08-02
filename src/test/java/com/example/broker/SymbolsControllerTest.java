cc517b94ab83efc205c07aff1f4e15bf85c7c362
Fisnik Shabani <fisnik.shabani@gmail.com>
2022-08-02 02:14:04 +0200

    Query parameters & JUnit tests

--- src/test/java/com/example/broker/SymbolsControllerTest.java
+++ src/test/java/com/example/broker/SymbolsControllerTest.java
@@ -10,12 +10,16 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @MicronautTest
 class SymbolsControllerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SymbolsControllerTest.class);
 
     @Inject
     @Client("/symbols")
@@ -44,4 +48,24 @@ class SymbolsControllerTest {
         assertEquals(HttpStatus.OK, response.getStatus());
         assertEquals(testSymbol, response.getBody().get());
     }
+
+    @Test
+    void symbolsEndpointReturnsListOfSymbolTakingQueryParametersIntoAccount() {
+        var max10 = client.toBlocking().exchange("/filter?max=10", JsonNode.class);
+        assertEquals(HttpStatus.OK, max10.getStatus());
+        LOG.debug("Max: 10 {}", max10.getBody().get().size());
+        assertEquals(10, max10.getBody().get().size());
+
+        var offset7 = client.toBlocking().exchange("/filter?offset=7", JsonNode.class);
+        assertEquals(HttpStatus.OK, offset7.getStatus());
+        LOG.debug("Offset: 7 {}", offset7.getBody().get().size());
+        assertEquals(3, offset7.getBody().get().size());
+
+        var max2Offset7 = client.toBlocking().exchange("/filter?max=2&offset=7", JsonNode.class);
+        assertEquals(HttpStatus.OK, max2Offset7.getStatus());
+        LOG.debug("Max 2, Offset: 7 {}", max2Offset7.getBody().get().size());
+        assertEquals(2, max2Offset7.getBody().get().size());
+    }
+
+
 }
